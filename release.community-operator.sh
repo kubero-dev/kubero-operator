ae8b5e32647f4359553bb6d48dd66ea46c1696cd
Gianni Carafa <gianni.carafa@srf.ch>
2023-12-21 15:36:54 +0100

    add OLM Community operator release process

---
+++ release.community-operator.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+export VERSION=$(cat VERSION)
+
+cd ../community-operators
+
+ls -altr ./operators/kubero-operator
+echo "Enter the old version of the operator (e.g. 0.0.1):"
+read OLD_VERSION
+git co main
+git pull
+git branch add-upgrade-kubero-$VERSION
+git co add-upgrade-kubero-$VERSION
+
+cp -r ../kubero-operator/bundle ./operators/kubero-operator/$VERSION
+echo "  replaces: kubero-operator.v$OLD_VERSION" >> ./operators/kubero-operator/$VERSION/kubero-operator.clusterserviceversion.yaml
+
+#git add ./operators/kubero-operator/$VERSION
+#git commit -s -m "operator kubero-operator ($VERSION)"
+#git push --set-upstream origin add-upgrade-kubero-$VERSION
