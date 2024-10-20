b2bcb01a9636b6681919a4980f3c2ac2e40515df
Gianni Carafa <gianni.carafa@srf.ch>
2024-10-20 13:38:44 +0200

    Add and push the changes for releasing the community operator with upgraded version of Kubero-operator.

--- release.community-operator.sh
+++ release.community-operator.sh
@@ -16,6 +16,6 @@ git co add-upgrade-kubero-$VERSION
 cp -r ../kubero-operator/bundle ./operators/kubero-operator/$VERSION
 echo "  replaces: kubero-operator.v$OLD_VERSION" >> ./operators/kubero-operator/$VERSION/manifests/kubero-operator.clusterserviceversion.yaml
 
-#git add .
-#git commit -s -m "operator kubero-operator ($VERSION)"
-#git push --set-upstream origin add-upgrade-kubero-$VERSION
+git add .
+git commit -s -m "operator kubero-operator ($VERSION)"
+git push --set-upstream origin add-upgrade-kubero-$VERSION
